6d8fa656ec0d73cc0da63df66b40d14577ff8f39
baraon <gallegosbaraon@yahoo.com>
2017-06-07 21:20:34 -0500

    UI Mock Up for clients page

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -1,14 +1,241 @@
 @extends('layouts.dashboard')
 
 @section('css')
-
 @endsection
 
 @section('content')
-    <h2>Clients</h2>
+    <div class="row">
+    <div class="timer-container">
+
+    </div>
+    </div>
+    <br>
+    <div class="row">
+    <div class="timer-container">
+        <div class="row">
+            <div class="col-xs-4 col-md-4 timer-description">
+                <h1>A Cool Company</h1>
+                <p>123 Roadway Drive, San Antonio, TX</p>
+            </div>
+            <div class="col-xs-4 col-md-4 timer-description">
+                <h2>Contact</h2>
+                <p>Jerry Seinfeld</p>
+                <p>jerry.seinfeld@gmail.com</p>
+                <p>(555) 555-5555</p>
+            </div>
+            <div class="col-xs-4 col-md-4 timer-description">
+                <p>Currently there are <strong>4 active projects</strong> and <strong>2 workspaces</strong> being used</p>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+             <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+             <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <br>
+        <div class="row">
+    <div class="timer-container">
+        <div class="row">
+            <div class="col-xs-4 col-md-4 timer-description">
+                <h1>A Cool Company</h1>
+                <p>123 Roadway Drive, San Antonio, TX</p>
+            </div>
+            <div class="col-xs-4 col-md-4 timer-description">
+                <h2>Contact</h2>
+                <p>Jerry Seinfeld</p>
+                <p>jerry.seinfeld@gmail.com</p>
+                <p>(555) 555-5555</p>
+            </div>
+            <div class="col-xs-4 col-md-4 timer-description">
+                <p>Currently there are <strong>4 active projects</strong> and <strong>2 workspaces</strong> being used</p>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+             <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+             <div class="col-xs-4 col-md-4 timer-description">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-md-6">
+                        <p><strong>Project A</strong></p>
+                        <p><strong>Workspace:</strong> Department A</p>
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <p><a>Manage Project</a></p>
+                            <p><a>Manage Workspace</a></p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <p><strong>Start Date:</strong> June 4, 2017 <strong>End Date:</strong> June 26, 2017</p>
+                        <p><strong>Projected Cost:</strong> $1,200.00</p>
+                        <p><strong>Projected Time:</strong> 120 hours</p>
+                        <p><strong>Current Time:</strong> 67 hours 46 minutes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <br>
+    
 @endsection
 
 
 @section('js')
-
+    
 @endsection
