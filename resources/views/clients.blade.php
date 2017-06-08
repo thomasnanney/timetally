9f47f2e082036f0fcb2ea0e46b930ff5c11d6028
Kyle Widmann <Kyle.Widmann@gmail.com>
2017-06-08 00:38:25 -0500

    Fixed activity sidebar in dashboard and started adding menu to clients

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -7,14 +7,62 @@
     <div class="row">
     <div class="timer-container">
         <div class="row">
-            <div class="col-md-2 text-left timer-description">
+            <div class="col-md-2 text-left ">
                 <h1>Clients</h1>
             </div>
-        <div class="col-md-8 timer-description">
+        <div class="col-md-8 ">
             <input type="text" placeholder="Search Clients...">
         </div>
-            <div class="col-md-2 timer-description">
-                <button type="button" class="btn">Add Client</button>
+            <div class="col-md-2 tk-dropdown-container relative">
+                <button type="button" class="btn tk-dropdown-toggle">Add Client</button>
+                <div class="tk-dropdown set-project-dropdown tk-root">
+                    <div class="tk-search-projects-dropdown ">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <input type="text" placeholder="Search projects...">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <ul class="tk-projects-found">
+                                    <li class="tk-project-name">Project 1</li>
+                                    <li class="tk-project-name">Project 2</li>
+                                    <li class="tk-project-name">Project 3</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="tk-add-new-project text-center">
+                                    <a href="#">Create New Project +</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tk-add-new-project-dropdown hidden">
+                        <form>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input type="text" placeholder="Company Name...">
+                                </div>
+                                <div class="input-group">
+                                    <input type="text" placeholder="Project Name...">
+                                </div>
+                            </div>
+                            <div class="input-group text-right">
+                                <button type="submit" class="btn btn-success">Save</button>
+                            </div>
+                        </form>
+                        <hr>
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="tk-add-new-project text-center">
+                                    <a href="#">Search Projects</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         </div>
@@ -23,23 +71,23 @@
     <div class="row">
     <div class="timer-container">
         <div class="row">
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <h1>A Cool Company</h1>
                 <p>123 Roadway Drive, San Antonio, TX</p>
             </div>
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <h2>Contact</h2>
                 <p>Jerry Seinfeld</p>
                 <p>jerry.seinfeld@gmail.com</p>
                 <p>(555) 555-5555</p>
             </div>
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <p>Currently there are <strong>4 active projects</strong> and <strong>2 workspaces</strong> being used</p>
             </div>
         </div>
         <hr>
         <div class="row">
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -61,7 +109,7 @@
                     </div>
                 </div>
             </div>
-             <div class="col-xs-4 col-md-4 timer-description">
+             <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -83,7 +131,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -105,7 +153,7 @@
                     </div>
                 </div>
             </div>
-             <div class="col-xs-4 col-md-4 timer-description">
+             <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -134,23 +182,23 @@
         <div class="row">
     <div class="timer-container">
         <div class="row">
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <h1>A Cool Company</h1>
                 <p>123 Roadway Drive, San Antonio, TX</p>
             </div>
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <h2>Contact</h2>
                 <p>Jerry Seinfeld</p>
                 <p>jerry.seinfeld@gmail.com</p>
                 <p>(555) 555-5555</p>
             </div>
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <p>Currently there are <strong>4 active projects</strong> and <strong>2 workspaces</strong> being used</p>
             </div>
         </div>
         <hr>
         <div class="row">
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -172,7 +220,7 @@
                     </div>
                 </div>
             </div>
-             <div class="col-xs-4 col-md-4 timer-description">
+             <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -194,7 +242,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-4 col-md-4 timer-description">
+            <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
@@ -216,7 +264,7 @@
                     </div>
                 </div>
             </div>
-             <div class="col-xs-4 col-md-4 timer-description">
+             <div class="col-xs-4 col-md-4 ">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                     <div class="row">
