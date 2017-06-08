9356ccf1c272d53e5a51b892ea06fb0229aa29af
Kyle Widmann <Kyle.Widmann@gmail.com>
2017-06-08 11:13:19 -0500

    Added in a temp chart to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
     <link href="{{asset('/css/dashboard.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{asset('/libs/chartist-js/dist/chartist.min.css')}}">
 @endsection
 
 @section('content')
@@ -13,7 +14,25 @@
     <hr>
     <div class="row">
         <div class="col-xs-12 col-md-8">
-
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2>Weekly Report</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <div class="chart-toggle-container">
+                        <span>Me  </span>
+                        <label class="switch">
+                            <input type="checkbox">
+                            <div class="slider round"></div>
+                        </label>
+                        <span>  Team</span>
+                    </div>
+                </div>
+            </div>
+            <div class="ct-chart ct-perfect-fourth">
+            </div>
         </div>
         <div class="col-xs-12 col-md-4 activity-sidebar">
             <div class="row">
@@ -99,5 +118,28 @@
 
 
 @section('js')
+    <script src="{{asset('/libs/chartist-js/dist/chartist.min.js')}}"></script>
+    <script type="text/javascript">
+        var data = {
+            // A labels array that can contain any sort of values
+            labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+            // Our series array that contains series objects or in this case series data arrays
+            series: [
+                [10, 12, 8, 6, 6],
+                [9, 8, 6, 1, 6]
+            ]
+        };
 
+        var options = {
+            height: 500
+        };
+
+        new Chartist.Bar('.ct-chart', data, options).on('draw', function(data) {
+            if (data.type === 'bar') {
+                data.element.attr({
+                    style: 'stroke-width: 50px'
+                });
+            }
+        });
+    </script>
 @endsection
