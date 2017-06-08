96b8ac9b74caf82593eb78690f3526ddaf1d19f7
baraon <gallegosbaraon@yahoo.com>
2017-06-07 23:48:47 -0500

    Dashboard done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,139 @@
 @endsection
 
 @section('content')
-    <h2>WELCOME</h2>
+    <div class="row">
+    <div class="timer-container">
+        <div class="row">
+            <div class="col-md-2 text-left timer-description">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+           
+            <div class="row">
+                <div class="col-sm-8">
+
+                </div>
+                <div class="col-sm-4 timer-container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h2>Most Contributions this Week</h2>
+                        <h1>Jerry Seinfeld</h2>
+                    </div>
+                    <div class="col-sm-8">
+                        <img src="{{asset('images/seinfeld3.png')}}">
+                    </div>
+
+                </div>
+                <br>
+                <h2>Activity</h2>
+                <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <p style="font-size: 14px"><strong>Jane Doe from <a>Department A</a> worked on <a>Project B</a></strong></p>
+                            <p style="font-size: 14px">Worked on fixing a problem with the thing</p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p>a day ago</p>
+                            <p>01:30:00</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        
+    </div>
 @endsection
 
 
