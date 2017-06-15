2a8d61f8a84f622d7efc947e856f1754b50b7c13
baraon <gallegosbaraon@yahoo.com>
2017-06-15 14:03:28 -0500

    Created Database Migrations

---
+++ database/migrations/2017_06_15_183922_create_timeentries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timeentries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('workspaceID');
+            $table->integer('projectID');
+            $table->timeTz('startTime');
+            $table->timeTz('endTime');
+            $table->string('title');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timekeeper');
+    }
+}
---
+++ database/migrations/2017_06_15_184017_create_client_organization_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientOrganizationPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_organization_pivot', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('clientID');
+            $table->integer('organizationID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_organization_pivot');
+    }
+}
