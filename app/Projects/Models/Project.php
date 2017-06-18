99753c4f6da394782e52a9265c315693cfea7d42
baraon <gallegosbaraon@yahoo.com>
2017-06-18 15:51:23 -0500

    Initial Project Controller Added
    Assuming use of AJAX

---
+++ app/Projects/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //set fillable and guarded
+    protected $fillable = array(
+        'title',
+        'description',
+        'billableType',
+        'projectedRevenue',
+        'startDate',
+        'endDate',
+        'projectedTime',
+    );
+
+    public function projectUsers() {
+        // TODO: return users assigned to projects
+
+    }
+}
