a163649a2cbfc9131d642b3c8893e99418062682
vagrant <vagrant@timekeeper>
2017-06-19 00:09:35 +0000

    Adding in project manager component

---
+++ app/Projects/resources/assets/js/components/ProjectsManager.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class ProejctsManager extends Component{
+
+    constructor(props){
+
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    render(){
+
+    }
+}
+
+if(document.getElementById('projectManager')){
+    ReactDOM.render(<ProejctsManager/>, document.getElementById('projectManager'));
+}
