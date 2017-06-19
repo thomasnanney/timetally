175a8db32f7ab54799da63aa5c14a5b6f84f1aea
rtuck2 <ryuutaizen@gmail.com>
2017-06-18 21:46:44 -0500

    CreateClient

--- app/Clients/Controllers/ClientsController.php
+++ app/Clients/Controllers/ClientsController.php
@@ -2,11 +2,10 @@
 
 namespace App\Clients\Controllers;
 
+
 use Illuminate\Http\Request;
 use App\Core\Controllers\Controller;
 use App\Clients\Models\Client;
-use App\Clients\Requests\CreateClient;
-use App\Clients\Requests\UpdateClient;
 
 class ClientsController extends Controller
 {
@@ -31,60 +30,42 @@ class ClientsController extends Controller
     }
 
 
-    public function find($id)
-    {
+    public function createClient(Request $request) {
 
-        $client = Client::find($id);
-
-        return $client;
-    }
-
-
-    public function create(CreateClient $request)
-    {
+        $this->validate($request, [                 //rules
+            'name' => 'required|string|min:1',
+            'email' => 'required|email',
+            'address1' => 'required|string|min:1',
+            'address2' => 'sometimes|string',
+            'city' => 'required|string|min:1',
+            'state' => 'required|string|min:1',
+            'postalCode' => 'required|digits:5',
+            'description' => 'nullable|string|min:1',
+        ],[                                                     //Error messages
+            'name.required' => 'Please enter a Company Name',
+            'email.required' => 'Please enter an E-Mail',
+            'address1.required' => 'Please enter an Address',
+            'address2.string' => 'Please enter an Address2',
+            'city.required' => 'Please enter a City',
+            'state.required' => 'Please enter a State',
+            'postalCode.required' => 'Please enter a Postal Code',
+            'description.required' => 'Please enter a Description',
+        ]);
+                // client information
         $client = new Client;
-
         $client->name = $request->input('name');
         $client->email = $request->input('email');
         $client->address1 = $request->input('address1');
-        $client->address2 = 'filler';
+        $client->address2 = $request->input('address2');
         $client->city = $request->input('city');
         $client->state = $request->input('state');
         $client->postalCode = $request->input('postalCode');
-        $client->description = $request->input('description');
-
         $client->save();
+        return response()->json([
+            'status' => 'Success',
+        ]);
 
-        return $client;
+
     }
 
-
-    public function update(UpdateClient $request, $id)
-    {
-
-        $client = Client::find($id);
-
-        $client->name = $request->input('name');
-        $client->email = $request->input('email');
-        $client->address1 = $request->input('address1');
-        $client->address2 = 'filler';
-        $client->city = $request->input('city');
-        $client->state = $request->input('state');
-        $client->postalCode = $request->input('postalCode');
-        $client->description = $request->input('description');
-
-        $client->save();
-
-        return $client;
-    }
-
-
-    public function delete($id)
-    {
-
-        $client = Client::find($id);
-
-        $client->delete();
-
-    }
 }
--- app/Clients/routes.php
+++ app/Clients/routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::get('clients', "ClientsController@index")->name('clients');
-Route::get('clients/{id}', "ClientsController@find");
-Route::post('clients', "ClientsController@create");
+//Route::get('clients/{id}', "ClientsController@find");
+Route::post('clients', "ClientsController@createClient");
 Route::put('/clients/{id}/update', "ClientsController@update");
 //Route::patch('/clients', "ClientsController@index")->name('clients');
-Route::delete('/clients/{id}/delete', "ClientsController@delete");
+//Route::delete('/clients/{id}/delete', "ClientsController@delete");
